695fded14663e916a25a73606e504dbcb9ace608
DPF0190 <domenick.fumai@gmail.com>
2016-07-12 15:04:29 +0000

    refactored.py

---
+++ refactored.py
@@ -0,0 +1,27 @@
+
+
+'''for the integers 1 through 50 find the sum of even numbers MINUS the sum of numbers that are divisible by 7'''
+
+#define even integers 1-50
+#define numbers divisible by 7
+#sum two parts
+#subtract numbers div by 7 from sum of even integers
+
+import math
+
+def get_even():
+    return list(range(0, 50, 2))
+
+def get_sevs():
+    return list(range(0, 50, 7))
+
+def solve_problem():
+    numa = get_even()
+    numsum = (sum(numa))
+    numb = get_sevs()
+    numsumb = (sum(numb))
+    return numsumb - numsum
+
+if __name__ == '__main__':
+       print(solve_problem())
+
